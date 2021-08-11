8187d320fedf6939b52de4f6727123bf1c21fab2
“bomboloni” <slarge1+github@gmail.com>
2021-08-10 19:39:02 -1000

    08-10 no reverse

---
+++ 08-10 no reverse.js
@@ -0,0 +1,20 @@
+// Given a string of characters as input, write a function that returns it with the characters reversed. No Reverse Method (well brute force it first, but then no reverse method)!
+
+const nr = (str) => {
+  let x = str.split('')
+  return [...x].map(x.pop, x).join('')
+}
+
+console.log(nr('hello'))
+
+// another  solution:
+
+// const nr = (str) => {
+//   let x = ''
+//   for (let char of str) {
+//     x = char + x
+//   }
+//   return x
+// }
+
+// console.log(nr('hello'))
