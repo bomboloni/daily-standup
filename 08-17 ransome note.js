a595fd0d1f9ead2d5f81393fc6366d37f0b1fa88
“bomboloni” <slarge1+github@gmail.com>
2021-08-16 21:14:25 -1000

    08-17 ransome note

--- 08-17 ransome note.js
+++ 08-17 ransome note.js
@@ -5,13 +5,35 @@
 const magazine =
   'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum'
 
-const ransom = (note) => {
-  // create an object of word : num
-  // loop through each word of the note to find it in the obejct. if it's there udate the value of the objects key. if it's not there return false
-  // return true
+const ransom = (note, mag) => {
+  let magWords = {}
+  let noteWords = {}
+  let regex = /[^a-z0-9\s']/g
+
+  // creates an object of word : num, filters out junk
+  const process = (str) => {
+    const obj = {}
+    str = str.toLowerCase().replace(regex, '').trim().split(' ')
+    for (let word of str) {
+      obj[word] = obj[word] + 1 || 1
+    }
+    return obj
+  }
+
+  noteWords = process(note)
+  magWords = process(mag)
+  const noteArr = Object.keys(noteWords)
+  let result = true
+
+  for (word of noteArr) {
+    if (noteWords[word] <= magWords[word] === false) {
+      result = false
+    }
+  }
+  return result
 }
 
-console.log(ransome('sit ad est sint'), true)
-console.log(ransome('sit ad est love'), false)
-console.log(ransome('sit ad est sint in in'), true)
-console.log(ransome('sit ad est sint in in in in'), false)
+console.log(ransom('sit ad est sint', magazine), true)
+console.log(ransom('sit ad est love', magazine), false)
+console.log(ransom('sit ad est sint in in', magazine), true)
+console.log(ransom('sit ad est sint in in in in', magazine), false)
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-My solutions for the [Learn With Leon](https://leonnoel.com/100devs/) 100Devs Daily Standups.
+My solutions for the [Learn With Leon](https://leonnoel.com/100devs/) (100Devs) Daily Standups.
