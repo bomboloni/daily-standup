da579f4def0d1d0b442ad3ebf56dbb493e064281
“bomboloni” <slarge1+github@gmail.com>
2021-08-17 10:29:01 -1000

    cleaned it up a little, gave things better names

--- 08-17 ransome note.js
+++ 08-17 ransome note.js
@@ -6,10 +6,9 @@ const magazine =
   'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum'
 
 const ransom = (note, mag) => {
-  const regex = /[^a-z0-9\s']/g
-
   // creates an object of word : num, filters out junk
-  const process = (str) => {
+  const objectifyStr = (str) => {
+    const regex = /[^a-z0-9\s']/g
     const obj = {}
     const arr = str.toLowerCase().replace(regex, '').trim().split(' ')
     for (word of arr) {
@@ -18,13 +17,13 @@ const ransom = (note, mag) => {
     return obj
   }
 
-  const noteWords = process(note)
-  const magWords = process(mag)
-  const noteArr = Object.keys(noteWords)
+  const noteObj = objectifyStr(note)
+  const magObj = objectifyStr(mag)
+  const noteArr = Object.keys(noteObj)
   let result = true
 
   for (word of noteArr) {
-    if (noteWords[word] <= magWords[word] === false) {
+    if (noteObj[word] <= magObj[word] === false) {
       result = false
     }
   }
