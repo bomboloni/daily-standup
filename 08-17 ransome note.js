8a51d51b78f3442d9e372dcb40ad1a542ad6eb81
“bomboloni” <slarge1+github@gmail.com>
2021-08-17 10:31:27 -1000

    comment edit

--- 08-17 ransome note.js
+++ 08-17 ransome note.js
@@ -6,7 +6,7 @@ const magazine =
   'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum'
 
 const ransom = (note, mag) => {
-  // creates an object of word : num, filters out junk
+  // creates an object of word : wordCount, filters out junk
   const objectifyStr = (str) => {
     const regex = /[^a-z0-9\s']/g
     const obj = {}
