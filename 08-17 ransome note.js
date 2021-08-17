d14ef41c505f58aa1182f728355077ca8994bfcc
“bomboloni” <slarge1+github@gmail.com>
2021-08-16 21:22:10 -1000

    cleaned up const/variable declarations

--- 08-17 ransome note.js
+++ 08-17 ransome note.js
@@ -6,8 +6,6 @@ const magazine =
   'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum'
 
 const ransom = (note, mag) => {
-  let magWords = {}
-  let noteWords = {}
   let regex = /[^a-z0-9\s']/g
 
   // creates an object of word : num, filters out junk
@@ -20,8 +18,8 @@ const ransom = (note, mag) => {
     return obj
   }
 
-  noteWords = process(note)
-  magWords = process(mag)
+  const noteWords = process(note)
+  const magWords = process(mag)
   const noteArr = Object.keys(noteWords)
   let result = true
 
