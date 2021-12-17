1b315030ebfc086547f8a52ba2e13fe0f66bb2e0
“bomboloni” <slarge1+github@gmail.com>
2021-12-17 12:25:51 -1000

    2021-12-17 12:25:51

---
+++ 12-16 memoization fib.js
@@ -0,0 +1,52 @@
+// The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, such that each number is the sum of the two preceding ones, starting from 0 and 1. That is,
+
+// F(0) = 0, F(1) = 1
+// F(n) = F(n - 1) + F(n - 2), for n > 1.
+// Given n, calculate F(n).
+
+// Example 1:
+
+// Input: n = 2
+// Output: 1
+// Explanation: F(2) = F(1) + F(0) = 1 + 0 = 1.
+// Example 2:
+
+// Input: n = 3
+// Output: 2
+// Explanation: F(3) = F(2) + F(1) = 1 + 1 = 2.
+// Example 3:
+
+// Input: n = 4
+// Output: 3
+// Explanation: F(4) = F(3) + F(2) = 2 + 1 = 3.
+
+// P - a number
+// R - a number
+// E - see console log
+// P - initialized a memo array
+//      set the concluding conditions
+//      loop through recursively, checking the memo array
+
+function memoFib(n) {
+  let memo = [0, 1]
+
+  const fib = (n) => {
+    let result
+
+    if (n < 2) {
+      result = memo[n]
+    } else {
+      result = fib(n - 2) + fib(n - 1)
+      memo[n] = result
+    }
+    return result
+  }
+
+  return fib(n)
+}
+
+// console.log(memoFib(1), 1)
+// console.log(memoFib(2), 1)
+// console.log(memoFib(3), 2)
+// console.log(memoFib(4), 3)
+console.log(memoFib(5))
---
+++ 12-17 recursive factorial.js
@@ -0,0 +1,27 @@
+// Factorial
+
+// In mathematics, the factorial of a non-negative integer n, denoted by n!, is the product of all positive integers less than or equal to n. For example,
+
+// 5! = 5 * 4 * 3 * 2 * 1 = 120.
+
+// The value of 0! is 1.
+
+// #Your task
+
+// You have to create the function factorial that receives n and returns n!. You have to use recursion.
+
+// P - a number
+// R - a number
+// E - see console logs
+// P - test for 0
+//      base case: n = 1
+//      initialize a result to 1 and make a loop that *=
+
+function fact(n) {
+  if (n === 0) return 1
+
+  return n * fact(n - 1)
+}
+
+console.log(fact(0), 1)
+console.log(fact(5), 120)
